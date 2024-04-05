11e52d15c1d3a405577b685f12b23516e6b1a38a
Fred Underwood <underwoodfred@proton.me>
2024-04-05 13:09:12 +1000

    update biometric second factor PIN title

--- packages/SystemUI/src/com/android/keyguard/KeyguardBiometricSecondFactorPinViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardBiometricSecondFactorPinViewController.java
@@ -5,6 +5,7 @@ import com.android.internal.util.LatencyTracker;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.systemui.classifier.FalsingCollector;
 import com.android.systemui.flags.FeatureFlags;
+import com.android.systemui.res.R;
 import com.android.systemui.statusbar.policy.DevicePostureController;
 import com.android.systemui.user.domain.interactor.SelectedUserInteractor;
 
@@ -25,6 +26,11 @@ public class KeyguardBiometricSecondFactorPinViewController extends KeyguardPinV
                 selectedUserInteractor, uiEventLogger);
         // TODO: Biometric second factor.
         //mPinLength = mLockPatternUtils.getPinLength(selectedUserInteractor.getSelectedUserId());
+    }
+
+    @Override
+    protected int getInitialMessageResId() {
+        return R.string.keyguard_enter_your_biometric_second_factor_pin;
     }
 
     @Override
