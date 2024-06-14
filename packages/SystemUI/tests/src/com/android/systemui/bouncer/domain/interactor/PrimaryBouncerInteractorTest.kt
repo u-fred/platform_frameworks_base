763bb0640e9d2e83daac289469027fc65dea6f97
Fred Underwood <underwoodfred@proton.me>
2024-06-14 13:11:35 +1000

    fix PrimaryBouncerInteractorTest incorrect args

--- packages/SystemUI/tests/src/com/android/systemui/bouncer/domain/interactor/PrimaryBouncerInteractorTest.kt
+++ packages/SystemUI/tests/src/com/android/systemui/bouncer/domain/interactor/PrimaryBouncerInteractorTest.kt
@@ -84,7 +84,7 @@ class PrimaryBouncerInteractorTest : SysuiTestCase() {
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
-        whenever(keyguardSecurityModel.getSecurityMode(anyInt(), eq(true)))
+        whenever(keyguardSecurityModel.getSecurityMode(anyInt(), eq(false)))
             .thenReturn(KeyguardSecurityModel.SecurityMode.PIN)
 
         DejankUtils.setImmediate(true)
@@ -165,7 +165,7 @@ class PrimaryBouncerInteractorTest : SysuiTestCase() {
     @Test
     fun testShow_isResumed() {
         whenever(repository.primaryBouncerShow.value).thenReturn(true)
-        whenever(keyguardSecurityModel.getSecurityMode(anyInt(), eq(true)))
+        whenever(keyguardSecurityModel.getSecurityMode(anyInt(), eq(false)))
             .thenReturn(KeyguardSecurityModel.SecurityMode.SimPuk)
 
         underTest.show(true)
